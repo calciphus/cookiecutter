f027e5d721625b59f6613472f7e828c92018829f
Brad Hubbard <brad@bradhubbard.net>
2013-01-29 01:15:11 -0800

    Added gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookiecutter::Application.config.secret_token = 'd1ee045ec48ae3c9021b7abefa4062b5bde00a1cdebf9f54b8450c4d6ae34dfd94408c531c84edfd4c096f5351011e5e0da46975000e4f9f8a2911f1cdc95a30'
